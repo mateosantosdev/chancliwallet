f999bff1d2b3f12ddc14ded5ced21fe8d7aa29b5
Mateo Santos <klianmail@gmail.com>
2026-05-23 12:15:24 +0200

    feat: add demo data

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -2,9 +2,10 @@ import { readFileSync, unlinkSync, writeFileSync } from "node:fs";
 import Papa, { UnparseConfig } from "papaparse";
 import { join } from "node:path";
 
+const DIR = "data-demo";
 export function readCSV<T>(filename: string) {
   try {
-    const path = join(process.cwd(), "data", filename);
+    const path = join(process.cwd(), DIR, filename);
     const content = readFileSync(path, "utf8");
     return Papa.parse(content, { header: true }).data as T;
   } catch (error) {
@@ -18,7 +19,7 @@ export function writeCSV<T>(
   config?: UnparseConfig,
 ) {
   try {
-    const path = join(process.cwd(), "data", filename);
+    const path = join(process.cwd(), DIR, filename);
     writeFileSync(path, Papa.unparse(data, config), "utf8");
   } catch (error) {
     throw error;
@@ -27,7 +28,7 @@ export function writeCSV<T>(
 
 export function appendCSV(filename: string, data: Record<string, string>) {
   try {
-    const path = join(process.cwd(), "data", filename);
+    const path = join(process.cwd(), DIR, filename);
     const content = readFileSync(path, "utf8");
     const csvContent = Papa.parse(content, { header: true }).data;
     csvContent.push(data);
@@ -39,7 +40,7 @@ export function appendCSV(filename: string, data: Record<string, string>) {
 
 export function removeCSV(filename: string) {
   try {
-    const path = join(process.cwd(), "data", filename);
+    const path = join(process.cwd(), DIR, filename);
     unlinkSync(path);
   } catch (error) {
     throw error;
@@ -48,7 +49,7 @@ export function removeCSV(filename: string) {
 
 export function getLatestRowFromCSV<T>(filename: string) {
   try {
-    const path = join(process.cwd(), "data", filename);
+    const path = join(process.cwd(), DIR, filename);
     const content = readFileSync(path, "utf8");
     const data = Papa.parse(content, { header: true }).data;
     return data.at(-1) as T;
